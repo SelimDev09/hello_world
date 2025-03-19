a7b8f14cae6e125df7153d81617bc3d4c7687182
selim <aouniselim09@proton.me>
2025-03-19 15:50:32 +0100

    Fin de l'exercice 2 déclaration du nom de planete

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,5 +1,9 @@
 public class HelloUniverse {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Universe!");
+        
+        int value = 8;
+        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de :" + value);
+        value = 9;
+        System.err.println("Il y a quelques années cependant, elles étaient au nombre de :" + value);
     }
 }
