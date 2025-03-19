5330c2d4088a0e7d3bae4c127357a70400e7803c
selim <aouniselim09@proton.me>
2025-03-19 17:53:28 +0100

    Deuxieme façon de faire : declaration Objet String

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,9 +1,10 @@
 public class HelloUniverse {
     public static void main(String[] args) throws Exception {
-        
+        String phraseOne = "Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de :";
         int value = 8;
-        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de :" + value);
+        System.out.println( phraseOne + value);
+        String phraseTwo = "Il y a quelques années cependant, elles étaient au nombre de :";
         value = 9;
-        System.err.println("Il y a quelques années cependant, elles étaient au nombre de :" + value);
+        System.err.println( phraseTwo + value);
     }
 }
