18a84ef017e1421de8802b31d4076dc153ae84eb
selim <aouniselim09@proton.me>
2025-03-19 15:41:51 +0100

    Déclaration de variable dans Java et spec

---
+++ src/variable.java
@@ -0,0 +1,13 @@
+public class variable {
+  public static void main(String[] args) {
+    //explication des variables
+    int myNum = 5; // Integer (whole number)
+    long   milliDepuis1997 =  1609920618000;  
+    long   milliDepuis1997Bis =  1609920618000l;           // Integer (whole number)
+    float myFloatNum = 5.99; // Floating point number
+    float myFloatNumBis = 5.99f; // Floating point number
+    double myFloat = 1.44; // Floating point number
+    char initialePrenom = 'k';
+    boolean javaEstFacile = false;
+  }
+}
