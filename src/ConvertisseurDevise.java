db67f1a6aa7258cda01284dd6c433b452412c3ac
selim <aouniselim09@proton.me>
2025-04-03 18:48:59 +0200

    Exercie ConvertisseurDevise

---
+++ src/ConvertisseurDevise.java
@@ -0,0 +1,9 @@
+public class ConvertisseurDevise {
+  public static void main(String[] args) {
+    int somme = 1500;
+    double taux = 1.22;
+    double result = somme * taux;
+
+    System.err.println(result);
+  }
+}
