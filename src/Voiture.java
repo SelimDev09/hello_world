ced1c658dba1e565cef492ea2b565e28e36975c2
selim <aouniselim09@proton.me>
2025-04-05 12:14:31 +0200

    Section 6 + exo codage 12-13

---
+++ src/Voiture.java
@@ -0,0 +1,14 @@
+public class Voiture {
+  
+  //Propriété
+  //Par default 0,0
+  int nbPortes;
+  // Par default = false
+  boolean automatique;
+  // Par default Null
+  String couleur;
+  // Par default char sans valeur /u000
+  char premeireLettrePlauqe;
+
+
+}
