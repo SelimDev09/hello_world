3a4c7caed6a4a99cd084727deb8a8ddd54f421e7
selim <aouniselim09@proton.me>
2025-04-05 10:10:38 +0200

    exercice codage 10

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -2,24 +2,25 @@ public class HelloUniverse {
     public static void main(String[] args) {
         
         
-        int value = 10;
+    for (int index = 7; index <=9; index++) {
 
-        switch (value) {
-            case 7:
+        switch (index) {
+                case 7:
                 System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
                 break;
-
-            case 8:
+                
+                case 8:
                 System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
                 break;
-            
-            case 9:
+                
+                case 9:
                 System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
                 break;
-        
-            default:
-            System.out.printf("Le programme ne peut pas fournir de résultat pour %d", value);
+                
+                default:
+                System.out.printf("Le programme ne peut pas fournir de résultat pour %d", index);
                 break;
+            }
         }
 
 
